f6683f5fb54acaa7d65d92fb437fe860e5a81813
Martin <martin.foerster@hkc-home.de>
2015-03-28 00:15:31 +0100

    switched to the current hapi 8 api without good-squeeze

--- src/lib.js
+++ src/lib.js
@@ -1,36 +1,63 @@
 // Load modules
 
 var os = require('os');
+var domain = require("domain");
 var Hoek = require('hoek');
 var Stringify = require('json-stringify-safe');
 var Squeeze = require('good-squeeze').Squeeze;
 var lumberjack = require('lumberjack-protocol');
+var GoodReporter = require('good-reporter');
 
-module.exports = function (events, options) {
+var GoodLogstash = function (events, options) {
+    this.options = options;
+    GoodReporter.call(this, events, options);
+};
 
-    var client;
-    var squeezeStream = Squeeze(events);
-    
-    var GoodLogstash = function(events, options) {
-        
-    };
-    
-    var send = function(data) {
-        client.writeDataFrame({"line": Stringify(data)});
-    };
-    
-    var public = GoodLogstash.prototype;
-    
-    public.init = function(stream, emitter, callback) {
-        client = lumberjack.client(options.tlsOptions, options.clientOptions);
-        
-        squeezeStream.on('data', function (data) {
-            send(data);
-        });
+module.exports = GoodLogstash;
 
-        stream.pipe(squeezeStream);
-        callback();
-    };
-    
-    return new GoodLogstash();
+Hoek.inherits(GoodLogstash, GoodReporter);
+
+var public = GoodLogstash.prototype;
+
+public.send = function (data) {
+    this.client.writeDataFrame({
+        "line": Stringify(data)
+    });
+};
+
+public.start = function (emitter, callback) {
+
+    var onReport = this._handleEvent.bind(this);
+    var self = this;
+
+    emitter.on('report', onReport);
+
+    //errors from the most inner eventemitter of the tls socket implementation must be caught
+    //with this domain in order to prevent app crashes, when the receiving end resets the connection
+    //this can happen when the provided ssl cert is not valid for the corresponding server -> errors with socket hang up
+    var d = domain.create();
+    d.on("error", function (e) {
+        console.log("Caught error while connecting to logstashserver", e.message);
+
+    });
+    d.run(function () {
+        self.client = lumberjack.client(self.options.tlsOptions, self.options.clientOptions);
+    });
+
+    this.client.on("connect", function () {
+        console.log("connection with logstash server established");
+    });
+
+    this.client.on("disconnect", function () {
+        console.log("logstash client disconnected");
+    });
+
+    callback();
+};
+
+public._report = function (event, eventData) {
+    var tags = (eventData.tags || []).concat([]);
+    tags.unshift(event);
+
+    this.send(eventData);
 };
